7a720c398ac76992e2628856a00cb61689c6457e
tony-zeng <tony-zeng@DESKTOP-G6VPKTF.localdomain>
2018-11-21 02:03:38 -0500

    working on stuff

--- pages/MenuScreen.js
+++ pages/MenuScreen.js
@@ -43,7 +43,7 @@ export default class MenuScreen extends React.Component {
   fetchAccounts = () => {
     return new Promise((resolve, reject) => {
       this._retrieveData()
-        .then(accounts => {
+        .then( accounts => {
           this.setState(accounts, resolve);
         })
         .catch(e => {
